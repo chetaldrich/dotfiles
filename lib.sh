26be5d4c3f4e6b3a3cede1b999d98ca2a611ecfa
chetaldrich <chetaldrich@gmail.com>
2015-04-06 19:07:18 -0500

    Add initial install prompt

---
+++ lib.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+src=$HOME/dotfiles/src
+scripts=$HOME/dotfiles/scripts
+
+ESC_SEQ="\x1b["
+COL_CYAN=$ESC_SEQ"36;01m"
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+
+function bird(){
+    echo -e "\n$COL_CYAN(o$COL_YELLOW>$COL_RESET $1$COL_CYAN\n/))$COL_RESET\n"
+}
+
+function run(){
+    echo -en "$COL_YELLOW ⇒ $COL_RESET"$1": "
+}
+
+function install(){
+    bird "Installing various command line tools..."
+}
--- update.sh
+++ update.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 # Chet Aldrich
-# An install script for dotfiles 
+# An install script for dotfiles
 # that creates a symlink for each
 # necessary folder.
 
+chmod +x scripts/*
+chmod +x lib.sh
+source ./lib.sh
 
+bird "Hello! Welcome to the interactive install prompt!"
+
+read -r -p "Would you like to run the automated installation? (y/*)" response
+
+if [[ $response =~ ^(yes|y|Y) ]];then
+    install
+else
+    bird "Ok. Some other time."
+fi
