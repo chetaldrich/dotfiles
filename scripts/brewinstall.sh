f898e09eda1953313383874eac3f483a865a7d20
chetaldrich <chetaldrich@gmail.com>
2015-04-08 01:50:46 -0500

    Added desktop application installation

--- scripts/brewinstall.sh
+++ scripts/brewinstall.sh
@@ -6,7 +6,7 @@ fi
 
 # COMMAND LINE TOOLS
 # Insert command line tools to be installed by brew here:
-CLI=( git ruby python3 node plt-racket gcc )
+CLI=( caskroom/cask/brew-cask git ruby python3 node plt-racket gcc )
 
 for item in "${CLI[@]}"
 do
@@ -16,3 +16,21 @@ do
         brew upgrade $item
     fi
 done
+
+# GUI APPLICATIONS
+
+# Insert command line tools to be installed by brew here:
+GUI=( atom google-chrome )
+
+read -r -p "Would you like to add desktop applications using Cask? (y/*)" response
+
+if [[ $response =~ ^(yes|y|Y) ]];then
+    for item in "${GUI[@]}"
+    do
+        if [ -z "$(which $item)" ];then
+            brew cask install $item
+        fi
+    done
+else
+    echo "Ok. Those won't be installed."
+fi
