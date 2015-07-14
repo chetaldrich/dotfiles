68b33e2e77f0cf8d6bd47f8f5f68d62e3f08b115
chetaldrich <chetaldrich@gmail.com>
2015-07-14 00:43:46 -0700

    Added some small things
    Mostly some configuration stuff to make Atom more happy

--- scripts/apm.sh
+++ scripts/apm.sh
@@ -2,8 +2,19 @@
 # Chet Aldrich
 # Installs Atom text editor packages
 
+apm install auto-detect-indentation
+apm install color-picker
+apm install haskell-ghc-mod
+apm install ide-haskell
+apm install language-haml
+apm install language-haskell
+apm install language-latex
+apm install language-scala
+apm install latex
+apm install linter
+apm install linter-javac
 apm install minimap
+apm install pdf-view
+apm install tabs-to-spaces
 apm install terminal-status
 apm install vim-mode
-apm install language-scala
-apm install language-go
