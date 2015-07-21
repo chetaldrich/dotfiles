7efd8b437a8e7e5b7be7135dac612a11e7c7e53d
chetaldrich <chetaldrich@gmail.com>
2015-07-20 23:21:06 -0700

    Added some safety configurations to rm

--- lib.sh
+++ lib.sh
@@ -73,7 +73,7 @@ function minstall(){
     ./scripts/apm.sh
     bird "Adding and symlinking vim configurations..."
     ./scripts/linkvim.sh
-    bird "Adding shell configurations..."
+    bird "Adding additional configurations..."
     ./scripts/m_link_config.sh
 }
 
--- scripts/m_link_config.sh
+++ scripts/m_link_config.sh
@@ -4,3 +4,4 @@ source ../lib.sh
 
 # links miscellaneous configuration files for Mac
 prompt_install_home zshrc msrc
+npm install --global trash
