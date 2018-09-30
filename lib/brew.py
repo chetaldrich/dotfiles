5827aabc1fd088681f2ca9139b4a506e7d1481f7
Chet Aldrich <chetaldrich@gmail.com>
2018-09-29 23:42:00 -0400

    add some logic around whether something is already installed
    Should save on some bash calls only to figure out that the package is already there.

--- lib/brew.py
+++ lib/brew.py
@@ -1,15 +1,26 @@
 import subprocess as sp
 
-def install(package_name: str):
-    sp.run(['brew', 'install', package_name])
+def install(package: str):
+    if is_installed(package):
+        print("Already installed brew package {}. Skipping...".format(package))
+    else:
+        sp.run(['brew', 'install', package])
 
 def uninstall(package: str):
     sp.run(['brew', 'uninstall', package])
 
-def cask_install(package_name: str):
-    sp.run(['brew', 'cask', 'install', package_name])
+def cask_install(package: str):
+    if is_installed(package, cask=True):
+        print("Already installed brew package {}. Skipping...".format(package))
+    else:
+        sp.run(['brew', 'cask', 'install', package_name])
 
-def is_installed(package_name: str) -> bool:
-    output = sp.run(['brew', 'ls', '--versions', package_name],
+def is_installed(package_name: str, cask=False) -> bool:
+    """Given a package, determine whether brew has it installed or not."""
+    command = ['brew', 'ls', '--versions', package_name]
+    if cask:
+        command.insert(1, 'cask')
+
+    output = sp.run(command,
                     capture_output=True).stdout.decode('utf-8')
     return output != ''
