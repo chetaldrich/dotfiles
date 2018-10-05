07d97fe676bb0adb4a6d2afd111a4c8f40873554
Chet Aldrich <chetaldrich@gmail.com>
2018-10-04 23:34:10 -0400

    remove some old code, list plugins

--- run_installation.py
+++ run_installation.py
@@ -2,15 +2,17 @@ import lib.atom as atom
 import lib.brew as brew
 import lib.config as config
 
-def setup_atom():
-    plugin = brew.BrewPlugin()
-    plugin.cask_install('atom')
-    atom.install_atom_packages()
+def plugins():
+    return [
+        brew.BrewPlugin(),
+        atom.AtomPlugin()
+    ]
 
 def main():
     configuration = config.Configuration()
-    configuration.register(brew.BrewPlugin())
-    configuration.register(atom.AtomPlugin())
+    for plugin in plugins():
+        configuration.register(plugin)
+
     configuration.apply()
 
 if __name__ == '__main__':
