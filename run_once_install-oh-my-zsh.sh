5e6aece4a304fc40f23b9679af36046ebd2aad41
Chet Aldrich <chetaldrich@gmail.com>
2026-06-10 18:57:00 -0400

    ohmyzsh

---
+++ run_once_install-oh-my-zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Installs oh-my-zsh if not already present
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
